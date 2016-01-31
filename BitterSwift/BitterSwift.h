8a75e485f7ea16e51f43ab8a75aa92757971edf3
Umberto Raimondi <me@uraimo.com>
2016-01-31 22:20:58 +0100

    Creating project

---
+++ BitterSwift/BitterSwift.h
@@ -0,0 +1,19 @@
+//
+//  BitterSwift.h
+//  BitterSwift
+//
+//  Created by Umberto Raimondi on 31/01/16.
+//  Copyright © 2016 Umberto Raimondi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BitterSwift.
+FOUNDATION_EXPORT double BitterSwiftVersionNumber;
+
+//! Project version string for BitterSwift.
+FOUNDATION_EXPORT const unsigned char BitterSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BitterSwift/PublicHeader.h>
+
+
