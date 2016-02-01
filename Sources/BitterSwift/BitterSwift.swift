d3c1f5c55c9e2f178e0e0bff45c4b370a09194f4
Umberto Raimondi <me@uraimo.com>
2016-02-01 18:41:59 +0100

    Added size property and subscript for all integers

--- Sources/BitterSwift/BitterSwift.swift
+++ Sources/BitterSwift/BitterSwift.swift
@@ -22,6 +22,23 @@ extension Int{
         return Int64(self) //No difference if the platform is 32 or 64
         }
     public static var allOnes:Int{return Int(bitPattern: UInt.max)}
+    public static var size:Int{return strideof(self)}
+    subscript(index: Int) -> Int {
+        get {
+            if(Int.size == 8){
+                return Int(bitPattern: UInt((self.toU64 & (0xFF << index.toU64)) >> index.toU64) )
+            }else{
+               return Int(bitPattern: UInt((self.toU32 & (0xFF << index.toU32)) >> index.toU32) )
+            }
+        }
+        set(newValue) {
+            if(Int.size == 8){
+                self = Int(bitPattern: UInt(self.toU64 | (newValue.toU64 << index.toU64)) )
+            }else{
+                self = Int(bitPattern: UInt(self.toU32 | (newValue.toU32 << index.toU32)) )
+            }
+        }
+    }
 }
 extension UInt{
     public var toU8: UInt8{return UInt8(truncatingBitPattern:self)}
@@ -37,6 +54,23 @@ extension UInt{
         return Int64(self) //No difference if the platform is 32 or 64
         }
     public static var allOnes:UInt{return UInt.max}
+    public static var size:Int{return strideof(self)}
+    subscript(index: Int) -> UInt {
+        get {
+            if(Int.size == 8){
+                return UInt(self.toU64 & (0xFF << index.toU64) >> index.toU64)
+            }else{
+                return UInt(self.toU32 & (0xFF << index.toU32) >> index.toU32)
+            }
+        }
+        set(newValue) {
+            if(Int.size == 8){
+                self = UInt(self.toU64 | (newValue.toU64 << index.toU64))
+            }else{
+                self = UInt(self.toU32 | (newValue.toU32 << index.toU32))
+            }
+        }
+    }
 }
 
 extension Int8 {
@@ -53,6 +87,15 @@ extension Int8 {
         return Int64(self) //No difference if the platform is 32 or 64
         }
     public static var allOnes:Int8{return Int8(bitPattern: UInt8.max)}
+    public static var size:Int{return strideof(self)}
+    subscript(index: Int) -> Int8 {
+        get {
+            return ((self.toU8 & (0xFF << index.toU8)) >> index.toU8).to8
+        }
+        set(newValue) {
+            self = (self.toU8 | (newValue.toU8 << index.toU8)).to8
+        }
+    }
 }
 extension UInt8 {
     public var toU8: UInt8{return self}
@@ -68,6 +111,15 @@ extension UInt8 {
         return Int64(self) //No difference if the platform is 32 or 64
         }
     public static var allOnes:UInt8{return UInt8.max}
+    public static var size:Int{return strideof(self)}
+    subscript(index: Int) -> UInt8 {
+        get {
+            return self & (0xFF << index.toU8) >> index.toU8
+        }
+        set(newValue) {
+            self = self | (newValue.toU8 << index.toU8)
+        }
+    }
 }
 
 extension Int16 {
@@ -84,6 +136,15 @@ extension Int16 {
         return Int64(self) //No difference if the platform is 32 or 64
         }
     public static var allOnes:Int16{return Int16(bitPattern: UInt16.max)}
+    public static var size:Int{return strideof(self)}
+    subscript(index: Int) -> Int16 {
+        get {
+            return ((self.toU16 & (0xFF << index.toU16)) >> index.toU16).to16
+        }
+        set(newValue) {
+            self = (self.toU16 | (newValue.toU16 << index.toU16)).to16
+        }
+    }
 }
 extension UInt16 {
     public var toU8: UInt8{return UInt8(truncatingBitPattern:self)}
@@ -99,6 +160,15 @@ extension UInt16 {
         return Int64(self) //No difference if the platform is 32 or 64
         }
     public static var allOnes:UInt16{return UInt16.max}
+    public static var size:Int{return strideof(self)}
+    subscript(index: Int) -> UInt16 {
+        get {
+            return self & (0xFF << index.toU16) >> index.toU16
+        }
+        set(newValue) {
+            self = self | (newValue.toU16 << index.toU16)
+        }
+    }
 }
 
 extension Int32 {
@@ -115,6 +185,15 @@ extension Int32 {
         return Int64(self) //No difference if the platform is 32 or 64
         }
     public static var allOnes:Int32{return Int32(bitPattern: UInt32.max)}
+    public static var size:Int{return strideof(self)}
+    subscript(index: Int) -> Int32 {
+        get {
+            return ((self.toU32 & (0xFF << index.toU32)) >> index.toU32).to32
+        }
+        set(newValue) {
+            self = (self.toU32 | (newValue.toU32 << index.toU32)).to32
+        }
+    }
 }
 extension UInt32 {
     public var toU8: UInt8{ return UInt8(truncatingBitPattern:self)}
@@ -130,6 +209,15 @@ extension UInt32 {
         return Int64(self) //No difference if the platform is 32 or 64
         }
     public static var allOnes:UInt32{return UInt32.max}
+    public static var size:Int{return strideof(self)}
+    subscript(index: Int) -> UInt32 {
+        get {
+            return self & (0xFF << index.toU32) >> index.toU32
+        }
+        set(newValue) {
+            self = self | (newValue.toU32 << index.toU32)
+        }
+    }
 }
 
 extension Int64 {
@@ -146,6 +234,15 @@ extension Int64 {
         return self //No difference if the platform is 32 or 64
         }
     public static var allOnes:Int64{return Int64(bitPattern: UInt64.max)}
+    public static var size:Int{return strideof(self)}
+    subscript(index: Int) -> Int64 {
+        get {
+            return ((self.toU64 & (0xFF << index.toU64)) >> index.toU64).to64
+        }
+        set(newValue) {
+            self = (self.toU64 | (newValue.toU64 << index.toU64)).to64
+        }
+    }
 }
 extension UInt64 {
     public var toU8: UInt8{ return UInt8(truncatingBitPattern:self)}
@@ -161,6 +258,15 @@ extension UInt64 {
         return Int64(bitPattern:self) //No difference if the platform is 32 or 64
         }
     public static var allOnes:UInt64{return UInt64.max}
+    public static var size:Int{return strideof(self)}
+    subscript(index: Int) -> UInt64 {
+        get {
+            return self & (0xFF << index.toU64) >> index.toU64
+        }
+        set(newValue) {
+            self = self | (newValue.toU64 << index.toU64)
+        }
+    }
 }
 
 // MARK: Operators
