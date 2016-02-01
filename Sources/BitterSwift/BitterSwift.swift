e0e198bb99fbe4b100d05ed1cb13c7d784bcb53d
Umberto Raimondi <me@uraimo.com>
2016-02-01 18:16:46 +0100

    Removing implicit property gets

--- Sources/BitterSwift/BitterSwift.swift
+++ Sources/BitterSwift/BitterSwift.swift
@@ -9,157 +9,157 @@
 // MARK: Int types extensions
 
 extension Int{
-    public var toU8: UInt8{ get{return UInt8(truncatingBitPattern:self)} }
-    public var to8: Int8{ get{return Int8(truncatingBitPattern:self)} }
-    public var toU16: UInt16{get{return UInt16(truncatingBitPattern:self)}}
-    public var to16: Int16{get{return Int16(truncatingBitPattern:self)}}
-    public var toU32: UInt32{get{return UInt32(truncatingBitPattern:self)}}
-    public var to32: Int32{get{return Int32(truncatingBitPattern:self)}}
-    public var toU64: UInt64{get{
+    public var toU8: UInt8{return UInt8(truncatingBitPattern:self)}
+    public var to8: Int8{return Int8(truncatingBitPattern:self)}
+    public var toU16: UInt16{return UInt16(truncatingBitPattern:self)}
+    public var to16: Int16{return Int16(truncatingBitPattern:self)}
+    public var toU32: UInt32{return UInt32(truncatingBitPattern:self)}
+    public var to32: Int32{return Int32(truncatingBitPattern:self)}
+    public var toU64: UInt64{
         return UInt64(self) //No difference if the platform is 32 or 64
-        }}
-    public var to64: Int64{get{
+        }
+    public var to64: Int64{
         return Int64(self) //No difference if the platform is 32 or 64
-        }}
+        }
     public static var allOnes:Int{return Int(bitPattern: UInt.max)}
 }
 extension UInt{
-    public var toU8: UInt8{ get{return UInt8(truncatingBitPattern:self)} }
-    public var to8: Int8{ get{return Int8(truncatingBitPattern:self)} }
-    public var toU16: UInt16{get{return UInt16(truncatingBitPattern:self)}}
-    public var to16: Int16{get{return Int16(truncatingBitPattern:self)}}
-    public var toU32: UInt32{get{return UInt32(truncatingBitPattern:self)}}
-    public var to32: Int32{get{return Int32(truncatingBitPattern:self)}}
-    public var toU64: UInt64{get{
+    public var toU8: UInt8{return UInt8(truncatingBitPattern:self)}
+    public var to8: Int8{return Int8(truncatingBitPattern:self)}
+    public var toU16: UInt16{return UInt16(truncatingBitPattern:self)}
+    public var to16: Int16{return Int16(truncatingBitPattern:self)}
+    public var toU32: UInt32{return UInt32(truncatingBitPattern:self)}
+    public var to32: Int32{return Int32(truncatingBitPattern:self)}
+    public var toU64: UInt64{
         return UInt64(self) //No difference if the platform is 32 or 64
-        }}
-    public var to64: Int64{get{
+        }
+    public var to64: Int64{
         return Int64(self) //No difference if the platform is 32 or 64
-        }}
+        }
     public static var allOnes:UInt{return UInt.max}
 }
 
 extension Int8 {
-    public var toU8: UInt8{ get{return UInt8(bitPattern:self)} }
-    public var to8: Int8{ get{return self} }
-    public var toU16: UInt16{get{return UInt16(self)}}
-    public var to16: Int16{get{return Int16(self)}}
-    public var toU32: UInt32{get{return UInt32(self)}}
-    public var to32: Int32{get{return Int32(self)}}
-    public var toU64: UInt64{get{
+    public var toU8: UInt8{return UInt8(bitPattern:self)}
+    public var to8: Int8{return self}
+    public var toU16: UInt16{return UInt16(self)}
+    public var to16: Int16{return Int16(self)}
+    public var toU32: UInt32{return UInt32(self)}
+    public var to32: Int32{return Int32(self)}
+    public var toU64: UInt64{
         return UInt64(self) //No difference if the platform is 32 or 64
-        }}
-    public var to64: Int64{get{
+        }
+    public var to64: Int64{
         return Int64(self) //No difference if the platform is 32 or 64
-        }}
+        }
     public static var allOnes:Int8{return Int8(bitPattern: UInt8.max)}
 }
 extension UInt8 {
-    public var toU8: UInt8{ get{return self} }
-    public var to8: Int8{ get{return Int8(bitPattern:self)} }
-    public var toU16: UInt16{get{return UInt16(self)}}
-    public var to16: Int16{get{return Int16(self)}}
-    public var toU32: UInt32{get{return UInt32(self)}}
-    public var to32: Int32{get{return Int32(self)}}
-    public var toU64: UInt64{get{
+    public var toU8: UInt8{return self}
+    public var to8: Int8{return Int8(bitPattern:self)}
+    public var toU16: UInt16{return UInt16(self)}
+    public var to16: Int16{return Int16(self)}
+    public var toU32: UInt32{return UInt32(self)}
+    public var to32: Int32{return Int32(self)}
+    public var toU64: UInt64{
         return UInt64(self) //No difference if the platform is 32 or 64
-        }}
-    public var to64: Int64{get{
+        }
+    public var to64: Int64{
         return Int64(self) //No difference if the platform is 32 or 64
-        }}
+        }
     public static var allOnes:UInt8{return UInt8.max}
 }
 
 extension Int16 {
-    public var toU8: UInt8{ get{return UInt8(truncatingBitPattern:self)} }
-    public var to8: Int8{ get{return Int8(truncatingBitPattern:self)} }
-    public var toU16: UInt16{get{return UInt16(bitPattern:self)}}
-    public var to16: Int16{get{return self}}
-    public var toU32: UInt32{get{return UInt32(self)}}
-    public var to32: Int32{get{return Int32(self)}}
-    public var toU64: UInt64{get{
+    public var toU8: UInt8{return UInt8(truncatingBitPattern:self)}
+    public var to8: Int8{return Int8(truncatingBitPattern:self)}
+    public var toU16: UInt16{return UInt16(bitPattern:self)}
+    public var to16: Int16{return self}
+    public var toU32: UInt32{return UInt32(self)}
+    public var to32: Int32{return Int32(self)}
+    public var toU64: UInt64{
         return UInt64(self) //No difference if the platform is 32 or 64
-        }}
-    public var to64: Int64{get{
+        }
+    public var to64: Int64{
         return Int64(self) //No difference if the platform is 32 or 64
-        }}
+        }
     public static var allOnes:Int16{return Int16(bitPattern: UInt16.max)}
 }
 extension UInt16 {
-    public var toU8: UInt8{ get{return UInt8(truncatingBitPattern:self)} }
-    public var to8: Int8{ get{return Int8(truncatingBitPattern:self)} }
-    public var toU16: UInt16{get{return self}}
-    public var to16: Int16{get{return Int16(bitPattern:self)}}
-    public var toU32: UInt32{get{return UInt32(self)}}
-    public var to32: Int32{get{return Int32(self)}}
-    public var toU64: UInt64{get{
+    public var toU8: UInt8{return UInt8(truncatingBitPattern:self)}
+    public var to8: Int8{return Int8(truncatingBitPattern:self)}
+    public var toU16: UInt16{return self}
+    public var to16: Int16{return Int16(bitPattern:self)}
+    public var toU32: UInt32{return UInt32(self)}
+    public var to32: Int32{return Int32(self)}
+    public var toU64: UInt64{
         return UInt64(self) //No difference if the platform is 32 or 64
-        }}
-    public var to64: Int64{get{
+        }
+    public var to64: Int64{
         return Int64(self) //No difference if the platform is 32 or 64
-        }}
+        }
     public static var allOnes:UInt16{return UInt16.max}
 }
 
 extension Int32 {
-    public var toU8: UInt8{ get{return UInt8(truncatingBitPattern:self)} }
-    public var to8: Int8{ get{return Int8(truncatingBitPattern:self)} }
-    public var toU16: UInt16{get{return UInt16(truncatingBitPattern:self)}}
-    public var to16: Int16{get{return Int16(truncatingBitPattern:self)}}
-    public var toU32: UInt32{get{return UInt32(bitPattern:self)}}
-    public var to32: Int32{get{return self}}
-    public var toU64: UInt64{get{
+    public var toU8: UInt8{return UInt8(truncatingBitPattern:self)}
+    public var to8: Int8{return Int8(truncatingBitPattern:self)}
+    public var toU16: UInt16{return UInt16(truncatingBitPattern:self)}
+    public var to16: Int16{return Int16(truncatingBitPattern:self)}
+    public var toU32: UInt32{return UInt32(bitPattern:self)}
+    public var to32: Int32{return self}
+    public var toU64: UInt64{
         return UInt64(self) //No difference if the platform is 32 or 64
-        }}
-    public var to64: Int64{get{
+        }
+    public var to64: Int64{
         return Int64(self) //No difference if the platform is 32 or 64
-        }}
+        }
     public static var allOnes:Int32{return Int32(bitPattern: UInt32.max)}
 }
 extension UInt32 {
-    public var toU8: UInt8{ get{return UInt8(truncatingBitPattern:self)} }
-    public var to8: Int8{ get{return Int8(truncatingBitPattern:self)} }
-    public var toU16: UInt16{get{return UInt16(truncatingBitPattern:self)}}
-    public var to16: Int16{get{return Int16(truncatingBitPattern:self)}}
-    public var toU32: UInt32{get{return self}}
-    public var to32: Int32{get{return Int32(bitPattern:self)}}
-    public var toU64: UInt64{get{
+    public var toU8: UInt8{ return UInt8(truncatingBitPattern:self)}
+    public var to8: Int8{ return Int8(truncatingBitPattern:self)}
+    public var toU16: UInt16{return UInt16(truncatingBitPattern:self)}
+    public var to16: Int16{return Int16(truncatingBitPattern:self)}
+    public var toU32: UInt32{return self}
+    public var to32: Int32{return Int32(bitPattern:self)}
+    public var toU64: UInt64{
         return UInt64(self) //No difference if the platform is 32 or 64
-        }}
-    public var to64: Int64{get{
+        }
+    public var to64: Int64{
         return Int64(self) //No difference if the platform is 32 or 64
-        }}
+        }
     public static var allOnes:UInt32{return UInt32.max}
 }
 
 extension Int64 {
-    public var toU8: UInt8{ get{return UInt8(truncatingBitPattern:self)} }
-    public var to8: Int8{ get{return Int8(truncatingBitPattern:self)} }
-    public var toU16: UInt16{get{return UInt16(truncatingBitPattern:self)}}
-    public var to16: Int16{get{return Int16(truncatingBitPattern:self)}}
-    public var toU32: UInt32{get{return UInt32(truncatingBitPattern:self)}}
-    public var to32: Int32{get{return Int32(truncatingBitPattern:self)}}
-    public var toU64: UInt64{get{
+    public var toU8: UInt8{ return UInt8(truncatingBitPattern:self)}
+    public var to8: Int8{ return Int8(truncatingBitPattern:self)}
+    public var toU16: UInt16{return UInt16(truncatingBitPattern:self)}
+    public var to16: Int16{return Int16(truncatingBitPattern:self)}
+    public var toU32: UInt32{return UInt32(truncatingBitPattern:self)}
+    public var to32: Int32{return Int32(truncatingBitPattern:self)}
+    public var toU64: UInt64{
         return UInt64(bitPattern:self) //No difference if the platform is 32 or 64
-        }}
-    public var to64: Int64{get{
+        }
+    public var to64: Int64{
         return self //No difference if the platform is 32 or 64
-        }}
+        }
     public static var allOnes:Int64{return Int64(bitPattern: UInt64.max)}
 }
 extension UInt64 {
-    public var toU8: UInt8{ get{return UInt8(truncatingBitPattern:self)} }
-    public var to8: Int8{ get{return Int8(truncatingBitPattern:self)} }
-    public var toU16: UInt16{get{return UInt16(truncatingBitPattern:self)}}
-    public var to16: Int16{get{return Int16(truncatingBitPattern:self)}}
-    public var toU32: UInt32{get{return UInt32(truncatingBitPattern:self)}}
-    public var to32: Int32{get{return Int32(truncatingBitPattern:self)}}
-    public var toU64: UInt64{get{
+    public var toU8: UInt8{ return UInt8(truncatingBitPattern:self)}
+    public var to8: Int8{ return Int8(truncatingBitPattern:self)}
+    public var toU16: UInt16{return UInt16(truncatingBitPattern:self)}
+    public var to16: Int16{return Int16(truncatingBitPattern:self)}
+    public var toU32: UInt32{return UInt32(truncatingBitPattern:self)}
+    public var to32: Int32{return Int32(truncatingBitPattern:self)}
+    public var toU64: UInt64{
         return self //No difference if the platform is 32 or 64
-        }}
-    public var to64: Int64{get{
+        }
+    public var to64: Int64{
         return Int64(bitPattern:self) //No difference if the platform is 32 or 64
-        }}
+        }
     public static var allOnes:UInt64{return UInt64.max}
 }
 
