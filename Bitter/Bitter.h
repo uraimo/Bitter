0b1006cfb14f4d55525e403eb19f14e05312929e
Umberto Raimondi <me@uraimo.com>
2016-02-03 09:21:07 +0100

    Modified structure to fix broken Pod

---
+++ Bitter/Bitter.h
@@ -0,0 +1,19 @@
+//
+//  Bitter.h
+//  Bitter
+//
+//  Created by Umberto Raimondi on 03/02/16.
+//  Copyright © 2016 Umberto Raimondi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bitter.
+FOUNDATION_EXPORT double BitterVersionNumber;
+
+//! Project version string for Bitter.
+FOUNDATION_EXPORT const unsigned char BitterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bitter/PublicHeader.h>
+
+
