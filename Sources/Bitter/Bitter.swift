b4ca5b3e277c06e640b730d8b8312d9531277c59
Umberto Raimondi <me@uraimo.com>
2016-02-05 15:57:47 +0100

    Added tests and fixed bugs
    The set subscript di not work, catched the bug while writing test (...),
    fixed a few other thing that came up. These tests can run on both 32bit
    and 64bit.

--- Sources/Bitter/Bitter.swift
+++ Sources/Bitter/Bitter.swift
@@ -21,23 +21,25 @@ extension Int{
     public var to64: Int64{
         return Int64(self) //No difference if the platform is 32 or 64
         }
+    public var toInt:Int{return self}
+    public var toUInt:UInt{return UInt(bitPattern:self)}
     public static var allOnes:Int{return Int(bitPattern: UInt.max)}
     public static var size:Int{return strideof(self)}
     public subscript(index: Int) -> Int {
         get {
             precondition(index<Int.size,"Byte set index out of range")
             if(Int.size == 8){
-                return Int(bitPattern: UInt((self.toU64 & (0xFF << index.toU64)) >> index.toU64) )
+                return Int(bitPattern: UInt((self.toU64 & (0xFF << (index.toU64*8))) >> (index.toU64*8)) )
             }else{
-               return Int(bitPattern: UInt((self.toU32 & (0xFF << index.toU32)) >> index.toU32) )
+               return Int(bitPattern: UInt((self.toU32 & (0xFF << (index.toU32*8))) >> (index.toU32*8)) )
             }
         }
         set(newValue) {
             precondition(index<Int.size,"Byte set index out of range")
             if(Int.size == 8){
-                self = Int(bitPattern: UInt(self.toU64 | (newValue.toU64 << index.toU64)) )
+                self = Int(bitPattern: UInt((self.toU64 & ~(0xFF << (index.toU64*8))) | (newValue.toU64 << (index.toU64*8))) )
             }else{
-                self = Int(bitPattern: UInt(self.toU32 | (newValue.toU32 << index.toU32)) )
+                self = Int(bitPattern: UInt((self.toU32 & ~(0xFF << (index.toU32*8))) | (newValue.toU32 << (index.toU32*8))) )
             }
         }
     }
@@ -55,23 +57,25 @@ extension UInt{
     public var to64: Int64{
         return Int64(self) //No difference if the platform is 32 or 64
         }
+    public var toInt:Int{return Int(bitPattern:self)}
+    public var toUInt:UInt{return self}
     public static var allOnes:UInt{return UInt.max}
     public static var size:Int{return strideof(self)}
     public subscript(index: Int) -> UInt {
         get {
             precondition(index<Int.size,"Byte set index out of range")
             if(UInt.size == 8){
-                return UInt(self.toU64 & (0xFF << index.toU64) >> index.toU64)
+                return UInt(self.toU64 & (0xFF << (index.toU64*8))) >> (index*8).toUInt
             }else{
-                return UInt(self.toU32 & (0xFF << index.toU32) >> index.toU32)
+                return UInt(self.toU32 & (0xFF << (index.toU32*8))) >> (index*8).toUInt
             }
         }
         set(newValue) {
             precondition(index<Int.size,"Byte set index out of range")
             if(UInt.size == 8){
-                self = UInt(self.toU64 | (newValue.toU64 << index.toU64))
+                self = UInt((self.toU64 & ~(0xFF << (index.toU64*8))) | (newValue.toU64 << (index.toU64*8)))
             }else{
-                self = UInt(self.toU32 | (newValue.toU32 << index.toU32))
+                self = UInt((self.toU32 & ~(0xFF << (index.toU32*8))) | (newValue.toU32 << (index.toU32*8)))
             }
         }
     }
@@ -90,16 +94,18 @@ extension Int8 {
     public var to64: Int64{
         return Int64(self) //No difference if the platform is 32 or 64
         }
+    public var toInt:Int{return Int(bitPattern:UInt(self))}
+    public var toUInt:UInt{return UInt(self)}
     public static var allOnes:Int8{return Int8(bitPattern: UInt8.max)}
     public static var size:Int{return strideof(self)}
     public subscript(index: Int) -> Int8 {
         get {
             precondition(index<Int8.size,"Byte set index out of range")
-            return ((self.toU8 & (0xFF << index.toU8)) >> index.toU8).to8
+            return self
         }
         set(newValue) {
             precondition(index<Int8.size,"Byte set index out of range")
-            self = (self.toU8 | (newValue.toU8 << index.toU8)).to8
+            self = newValue
         }
     }
 }
@@ -116,16 +122,18 @@ extension UInt8 {
     public var to64: Int64{
         return Int64(self) //No difference if the platform is 32 or 64
         }
+    public var toInt:Int{return Int(bitPattern:UInt(self))}
+    public var toUInt:UInt{return UInt(self)}
     public static var allOnes:UInt8{return UInt8.max}
     public static var size:Int{return strideof(self)}
     public subscript(index: Int) -> UInt8 {
         get {
             precondition(index<Int8.size,"Byte set index out of range")
-            return self & (0xFF << index.toU8) >> index.toU8
+            return self
         }
         set(newValue) {
             precondition(index<Int8.size,"Byte set index out of range")
-            self = self | (newValue.toU8 << index.toU8)
+            self = newValue
         }
     }
 }
@@ -143,16 +151,18 @@ extension Int16 {
     public var to64: Int64{
         return Int64(self) //No difference if the platform is 32 or 64
         }
+    public var toInt:Int{return Int(bitPattern:UInt(self))}
+    public var toUInt:UInt{return UInt(self)}
     public static var allOnes:Int16{return Int16(bitPattern: UInt16.max)}
     public static var size:Int{return strideof(self)}
     public subscript(index: Int) -> Int16 {
         get {
             precondition(index<Int16.size,"Byte set index out of range")
-            return ((self.toU16 & (0xFF << index.toU16)) >> index.toU16).to16
+            return ( (self.toU16 & (0xFF << (index.toU16*8))) >> (index.toU16*8)).to16
         }
         set(newValue) {
             precondition(index<Int16.size,"Byte set index out of range")
-            self = (self.toU16 | (newValue.toU16 << index.toU16)).to16
+            self = ((self.toU16 & ~(0xFF << (index.toU16*8))) | (newValue.toU16 << (index.toU16*8))).to16
         }
     }
 }
@@ -169,16 +179,18 @@ extension UInt16 {
     public var to64: Int64{
         return Int64(self) //No difference if the platform is 32 or 64
         }
+    public var toInt:Int{return Int(bitPattern:UInt(self))}
+    public var toUInt:UInt{return UInt(self)}
     public static var allOnes:UInt16{return UInt16.max}
     public static var size:Int{return strideof(self)}
     public subscript(index: Int) -> UInt16 {
         get {
             precondition(index<Int16.size,"Byte set index out of range")
-            return self & (0xFF << index.toU16) >> index.toU16
+            return (self & (0xFF << (index.toU16*8))) >> (index.toU16*8)
         }
         set(newValue) {
             precondition(index<Int16.size,"Byte set index out of range")
-            self = self | (newValue.toU16 << index.toU16)
+            self = (self & ~(0xFF << (index.toU16*8))) | (newValue.toU16 << (index.toU16*8))
         }
     }
 }
@@ -196,16 +208,18 @@ extension Int32 {
     public var to64: Int64{
         return Int64(self) //No difference if the platform is 32 or 64
         }
+    public var toInt:Int{return Int(bitPattern:UInt(self))}
+    public var toUInt:UInt{return UInt(self)}
     public static var allOnes:Int32{return Int32(bitPattern: UInt32.max)}
     public static var size:Int{return strideof(self)}
     public subscript(index: Int) -> Int32 {
         get {
             precondition(index<Int32.size,"Byte set index out of range")
-            return ((self.toU32 & (0xFF << index.toU32)) >> index.toU32).to32
+            return ( (self.toU32 & (0xFF << (index.toU32*8))) >> (index.toU32*8)).to32
         }
         set(newValue) {
             precondition(index<Int32.size,"Byte set index out of range")
-            self = (self.toU32 | (newValue.toU32 << index.toU32)).to32
+            self = ((self.toU32 & ~(0xFF << (index.toU32*8))) | (newValue.toU32 << (index.toU32*8))).to32
         }
     }
 }
@@ -222,16 +236,18 @@ extension UInt32 {
     public var to64: Int64{
         return Int64(self) //No difference if the platform is 32 or 64
         }
+    public var toInt:Int{return Int(bitPattern:UInt(self))}
+    public var toUInt:UInt{return UInt(self)}
     public static var allOnes:UInt32{return UInt32.max}
     public static var size:Int{return strideof(self)}
     public subscript(index: Int) -> UInt32 {
         get {
             precondition(index<Int32.size,"Byte set index out of range")
-            return self & (0xFF << index.toU32) >> index.toU32
+            return (self & (0xFF << (index.toU32*8))) >> (index.toU32*8)
         }
         set(newValue) {
             precondition(index<Int32.size,"Byte set index out of range")
-            self = self | (newValue.toU32 << index.toU32)
+            self = (self & ~(0xFF << (index.toU32*8))) | (newValue.toU32 << (index.toU32*8))
         }
     }
 }
@@ -249,16 +265,18 @@ extension Int64 {
     public var to64: Int64{
         return self //No difference if the platform is 32 or 64
         }
+    public var toInt:Int{return Int(truncatingBitPattern:self)}
+    public var toUInt:UInt{return UInt(truncatingBitPattern:self)}
     public static var allOnes:Int64{return Int64(bitPattern: UInt64.max)}
     public static var size:Int{return strideof(self)}
     public subscript(index: Int) -> Int64 {
         get {
             precondition(index<Int64.size,"Byte set index out of range")
-            return ((self.toU64 & (0xFF << index.toU64)) >> index.toU64).to64
+            return ((self.toU64 & (0xFF << (index.toU64*8))) >> (index.toU64*8)).to64
         }
         set(newValue) {
             precondition(index<Int64.size,"Byte set index out of range")
-            self = (self.toU64 | (newValue.toU64 << index.toU64)).to64
+            self = ( (self.toU64 & ~(0xFF << (index.toU64*8))) | (newValue.toU64 << (index.toU64*8)) ).to64
         }
     }
 }
@@ -275,16 +293,18 @@ extension UInt64 {
     public var to64: Int64{
         return Int64(bitPattern:self) //No difference if the platform is 32 or 64
         }
+    public var toInt:Int{return Int(bitPattern:UInt(truncatingBitPattern:self))}
+    public var toUInt:UInt{return UInt(truncatingBitPattern:self)}
     public static var allOnes:UInt64{return UInt64.max}
     public static var size:Int{return strideof(self)}
     public subscript(index: Int) -> UInt64 {
         get {
             precondition(index<Int64.size,"Byte set index out of range")
-            return self & (0xFF << index.toU64) >> index.toU64
+            return (self & (0xFF << (index.toU64*8))) >> (index.toU64*8)
         }
         set(newValue) {
             precondition(index<Int64.size,"Byte set index out of range")
-            self = self | (newValue.toU64 << index.toU64)
+            self = (self & ~(0xFF << (index.toU64*8))) | (newValue.toU64 << (index.toU64*8))
         }
     }
 }
--- Sources/BitterTests/BitterTests.swift
+++ Sources/BitterTests/BitterTests.swift
@@ -20,13 +20,217 @@ class BitterTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+   
+    // MARK: UInt64 Tests
+    let iUInt64:UInt64 = (UInt.size==8) ? 0xFFAAFFAAFFAAFFAA : 0xFFAAFFAA
     func testUInt64Conversion() {
-        let i:UInt64=0xFFAAFFAAFFAAFFAA
-        XCTAssert(i.toU64==0xFFAAFFAAFFAAFFAA)
-        XCTAssert(i.toU32==0xFFAAFFAA)
-        XCTAssert(i.toU16==0xFFAA)
-        XCTAssert(i.toU8==0xAA)
+        XCTAssert(iUInt64.toUInt == UInt(truncatingBitPattern:iUInt64))
+        XCTAssert(iUInt64.toInt.toUInt == UInt(truncatingBitPattern:iUInt64))
+        XCTAssert(iUInt64.toU64 == iUInt64)
+        XCTAssert(iUInt64.to64.toU64 == iUInt64)
+        XCTAssert(iUInt64.toU32==0xFFAAFFAA)
+        XCTAssert(iUInt64.to32.toU32==0xFFAAFFAA)
+        XCTAssert(iUInt64.toU16==0xFFAA)
+        XCTAssert(iUInt64.to16.toU16==0xFFAA)
+        XCTAssert(iUInt64.toU8==0xAA)
+        XCTAssert(iUInt64.to8.toU8==0xAA)
+    }
+    let aUInt64:UInt64=0x1122334455667788
+    func testUInt64SubscriptGet(){
+        XCTAssertEqual(aUInt64[0], 0x88)
+        XCTAssertEqual(aUInt64[1], 0x77)
+        XCTAssertEqual(aUInt64[2], 0x66)
+        XCTAssertEqual(aUInt64[3], 0x55)
+        XCTAssertEqual(aUInt64[4], 0x44)
+        XCTAssertEqual(aUInt64[5], 0x33)
+        XCTAssertEqual(aUInt64[6], 0x22)
+        XCTAssertEqual(aUInt64[7], 0x11)
+    }
+    var asUInt64:UInt64=0x1122334455667788
+    func testUInt64SubscriptSet(){
+        asUInt64[0]=0xAA;asUInt64[1]=0xAA;asUInt64[2]=0xAA;
+        asUInt64[3]=0xAA;asUInt64[4]=0xAA;asUInt64[5]=0xAA;
+        asUInt64[6]=0xAA;asUInt64[7]=0xAA;
+        XCTAssertEqual(asUInt64[0], 0xAA)
+        XCTAssertEqual(asUInt64[1], 0xAA)
+        XCTAssertEqual(asUInt64[2], 0xAA)
+        XCTAssertEqual(asUInt64[3], 0xAA)
+        XCTAssertEqual(asUInt64[4], 0xAA)
+        XCTAssertEqual(asUInt64[5], 0xAA)
+        XCTAssertEqual(asUInt64[6], 0xAA)
+        XCTAssertEqual(asUInt64[7], 0xAA)
+    }
+     // MARK: Int64 Tests
+    let iInt64:Int64 = (Int.size==8) ? 0x6FAAFFAAFFAAFFAA : 0xFFAAFFAA
+    func testInt64Conversion() {
+        XCTAssert(iInt64.toUInt==UInt(truncatingBitPattern:iInt64))
+        XCTAssert(iInt64.toInt.toUInt==UInt(truncatingBitPattern:iInt64))
+        XCTAssert(iInt64.toU64==UInt64(bitPattern:iInt64))
+        XCTAssert(iInt64.to64.toU64==UInt64(bitPattern:iInt64))
+        XCTAssert(iInt64.toU32==0xFFAAFFAA)
+        XCTAssert(iInt64.to32.toU32==0xFFAAFFAA)
+        XCTAssert(iInt64.toU16==0xFFAA)
+        XCTAssert(iInt64.to16.toU16==0xFFAA)
+        XCTAssert(iInt64.toU8==0xAA)
+        XCTAssert(iInt64.to8.toU8==0xAA)
+    }
+    let aInt64:Int64=0x1122334455667788
+    func testInt64SubscriptGet(){
+        XCTAssertEqual(aInt64[0], 0x88)
+        XCTAssertEqual(aInt64[1], 0x77)
+        XCTAssertEqual(aInt64[2], 0x66)
+        XCTAssertEqual(aInt64[3], 0x55)
+        XCTAssertEqual(aInt64[4], 0x44)
+        XCTAssertEqual(aInt64[5], 0x33)
+        XCTAssertEqual(aInt64[6], 0x22)
+        XCTAssertEqual(aInt64[7], 0x11)
+    }
+    var asInt64:Int64=0x1122334455667788
+    func testInt64SubscriptSet(){
+        asInt64[0]=0xAA;asInt64[1]=0xAA;asInt64[2]=0xAA;
+        asInt64[3]=0xAA;asInt64[4]=0xAA;asInt64[5]=0xAA;
+        asInt64[6]=0xAA;asInt64[7]=0xAA;
+        XCTAssertEqual(asInt64[0], 0xAA)
+        XCTAssertEqual(asInt64[1], 0xAA)
+        XCTAssertEqual(asInt64[2], 0xAA)
+        XCTAssertEqual(asInt64[3], 0xAA)
+        XCTAssertEqual(asInt64[4], 0xAA)
+        XCTAssertEqual(asInt64[5], 0xAA)
+        XCTAssertEqual(asInt64[6], 0xAA)
+        XCTAssertEqual(asInt64[7], 0xAA)
+    }
+    
+    // MARK: UInt32 Tests
+    let iUInt32:UInt32=0xFFAAFFAA
+    func testUInt32Conversion() {
+        XCTAssert(iUInt32.toUInt==0xFFAAFFAA)
+        XCTAssert(iUInt32.toInt.toUInt==0xFFAAFFAA)
+        XCTAssert(iUInt32.toU64==0xFFAAFFAA)
+        XCTAssert(iUInt32.to64.toU64==0xFFAAFFAA)
+        XCTAssert(iUInt32.toU32==0xFFAAFFAA)
+        XCTAssert(iUInt32.to32.toU32==0xFFAAFFAA)
+        XCTAssert(iUInt32.toU16==0xFFAA)
+        XCTAssert(iUInt32.to16.toU16==0xFFAA)
+        XCTAssert(iUInt32.toU8==0xAA)
+        XCTAssert(iUInt32.to8.toU8==0xAA)
+    }
+    let aUInt32:UInt32=0x11667788
+    func testUInt32SubscriptGet(){
+        XCTAssertEqual(aUInt32[0], 0x88)
+        XCTAssertEqual(aUInt32[1], 0x77)
+        XCTAssertEqual(aUInt32[2], 0x66)
+        XCTAssertEqual(aUInt32[3], 0x11)
+    }
+    var asUInt32:UInt32=0x11667788
+    func testUInt32SubscriptSet(){
+        asUInt32[0]=0xAA;asUInt32[1]=0xAA;asUInt32[2]=0xAA;
+        asUInt32[3]=0xAA
+        XCTAssertEqual(asUInt32[0], 0xAA)
+        XCTAssertEqual(asUInt32[1], 0xAA)
+        XCTAssertEqual(asUInt32[2], 0xAA)
+        XCTAssertEqual(asUInt32[3], 0xAA)
+    }
+     // MARK: Int32 Tests
+    let iInt32:Int32=0x6FAAFFAA
+    func testInt32Conversion() {
+        XCTAssert(iInt32.toUInt==0x6FAAFFAA)
+        XCTAssert(iInt32.toInt.toUInt==0x6FAAFFAA)
+        XCTAssert(iInt32.toU64==0x6FAAFFAA)
+        XCTAssert(iInt32.to64.toU64==0x6FAAFFAA)
+        XCTAssert(iInt32.toU32==0x6FAAFFAA)
+        XCTAssert(iInt32.to32.toU32==0x6FAAFFAA)
+        XCTAssert(iInt32.toU16==0xFFAA)
+        XCTAssert(iInt32.to16.toU16==0xFFAA)
+        XCTAssert(iInt32.toU8==0xAA)
+        XCTAssert(iInt32.to8.toU8==0xAA)
+    }
+    let aInt32:Int32=0x11667788
+    func testInt32SubscriptGet(){
+        XCTAssertEqual(aInt32[0], 0x88)
+        XCTAssertEqual(aInt32[1], 0x77)
+        XCTAssertEqual(aInt32[2], 0x66)
+        XCTAssertEqual(aInt32[3], 0x11)
+    }
+    var asInt32:Int32=0x11667788
+    func testInt32SubscriptSet(){
+        asInt32[0]=0xAA;asInt32[1]=0xAA;asInt32[2]=0xAA;
+        asInt32[3]=0xAA
+        XCTAssertEqual(asInt32[0], 0xAA)
+        XCTAssertEqual(asInt32[1], 0xAA)
+        XCTAssertEqual(asInt32[2], 0xAA)
+        XCTAssertEqual(asInt32[3], 0xAA)
+    }
+    
+     // MARK: UInt16 Tests
+    let iUInt16:UInt16=0x6FAA
+    func testUInt16Conversion() {
+        XCTAssert(iUInt16.toUInt==0x6FAA)
+        XCTAssert(iUInt16.toInt.toUInt==0x6FAA)
+        XCTAssert(iUInt16.toU64==0x6FAA)
+        XCTAssert(iUInt16.to64.toU64==0x6FAA)
+        XCTAssert(iUInt16.toU32==0x6FAA)
+        XCTAssert(iUInt16.to32.toU32==0x6FAA)
+        XCTAssert(iUInt16.toU16==0x6FAA)
+        XCTAssert(iUInt16.to16.toU16==0x6FAA)
+        XCTAssert(iUInt16.toU8==0xAA)
+        XCTAssert(iUInt16.to8.toU8==0xAA)
+    }
+    let aUInt16:UInt16=0x1188
+    func testUInt16SubscriptGet(){
+        XCTAssertEqual(aUInt16[0], 0x88)
+        XCTAssertEqual(aUInt16[1], 0x11)
+    }
+    var asUInt16:UInt16=0x1188
+    func testUInt16SubscriptSet(){
+        asUInt16[0]=0xAA;asUInt16[1]=0xAA
+        XCTAssertEqual(asUInt16[0], 0xAA)
+        XCTAssertEqual(asUInt16[1], 0xAA)
+    }
+    // MARK: UInt8 Tests
+    let iUInt8:UInt8=0x66
+    func testUInt8Conversion() {
+        XCTAssert(iUInt8.toUInt==0x66)
+        XCTAssert(iUInt8.toInt.toUInt==0x66)
+        XCTAssert(iUInt8.toU64==0x66)
+        XCTAssert(iUInt8.to64.toU64==0x66)
+        XCTAssert(iUInt8.toU32==0x66)
+        XCTAssert(iUInt8.to32.toU32==0x66)
+        XCTAssert(iUInt8.toU16==0x66)
+        XCTAssert(iUInt8.to16.toU16==0x66)
+        XCTAssert(iUInt8.toU8==0x66)
+        XCTAssert(iUInt8.to8.toU8==0x66)
+    }
+    let aUInt8:UInt8=0x66
+    func testUInt8SubscriptGet(){
+        XCTAssertEqual(aUInt8[0], 0x66)
+    }
+    var asUInt8:UInt8=0x66
+    func testUInt8SubscriptSet(){
+        asUInt8[0]=0xAA
+        XCTAssertEqual(asUInt8[0], 0xAA)
+    }
+     // MARK: Int8 Tests
+    let iInt8:Int8=0x66
+    func testInt8Conversion() {
+        XCTAssert(iInt8.toUInt==0x66)
+        XCTAssert(iInt8.toInt.toUInt==0x66)
+        XCTAssert(iInt8.toU64==0x66)
+        XCTAssert(iInt8.to64.toU64==0x66)
+        XCTAssert(iInt8.toU32==0x66)
+        XCTAssert(iInt8.to32.toU32==0x66)
+        XCTAssert(iInt8.toU16==0x66)
+        XCTAssert(iInt8.to16.toU16==0x66)
+        XCTAssert(iInt8.toU8==0x66)
+        XCTAssert(iInt8.to8.toU8==0x66)
+    }
+    let aInt8:Int8=0x66
+    func testInt8SubscriptGet(){
+        XCTAssertEqual(aInt8[0], 0x66)
+    }
+    var asInt8:Int8=0x66
+    func testInt8SubscriptSet(){
+        asInt8[0]=0x1A
+        XCTAssertEqual(asInt8[0], 0x1A)
     }
     
 }
