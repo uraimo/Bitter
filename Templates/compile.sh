f1b65dd37bd77a6d363e661d660e9d9c2bcabe7a
Erik Strottmann <erik.strottmann@gmail.com>
2017-11-01 02:44:07 -0700

    Migrate the GYB template to Sourcery
    Closes #5.

--- Templates/compile.sh
+++ Templates/compile.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-wget https://github.com/apple/swift/raw/master/utils/gyb
-wget https://github.com/apple/swift/raw/master/utils/gyb.py
-chmod +x gyb
+wget https://github.com/krzysztofzablocki/Sourcery/releases/download/0.9.0/Sourcery-0.9.0.zip
+unzip Sourcery-0.9.0.zip -d Sourcery
 
-./gyb --line-directive '' -o ../Sources/Bitter/Bitter.swift Bitter.swift.gyb
+# Change directories to avoid a bug with relative paths containing .. in Sourcery 0.9.0
+# https://github.com/krzysztofzablocki/Sourcery/issues/380
+cd ..
+Templates/Sourcery/bin/sourcery
+cd Templates
 
-rm gyb
-rm gyb.py
-rm gyb.pyc
+rm Sourcery-0.9.0.zip
+rm -rf Sourcery
+# Remove an unnecessary file that Sourcery 0.9.0 creates on run
+# https://github.com/krzysztofzablocki/Sourcery/issues/421
+rm ../default.profraw
